b7f997015ba46e1b660e65eb9c0c3b3113999eeb
Den <52295780+josserden@users.noreply.github.com>
2022-07-18 10:16:15 +0300

    add lesson

--- js/task1.js
+++ js/task1.js
@@ -1,25 +1,29 @@
-//todo Основи об'єктів
+//todo Example 1 - Деструктуризація
 /*
- * Напиши скрипт, який, для об'єкта user, послідовно:
-
-* додає поле mood зі значенням 'happy' ✅
-* замінює значення hobby на 'skydiving' ✅
-* замінює значення premium на false ✅
-* виводить вміст об'єкта user у форматі ключ: значення використовуючи Object.keys() і for...of ✅
+ * Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору незалежних аргументів.
  */
 
-const user = {
-  name: 'John',
-  age: 21,
-  hobby: 'css',
-  premium: true,
-  location: { city: 'Kyiv' },
-};
+// Було
+// console.log(calcBMI('88,3', '1.75'));
+// console.log(calcBMI('68,3', '1.65'));
+// console.log(calcBMI('118,3', '1.95'));
 
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
-
-for (const key of Object.keys(user)) {
-  console.log(`${key} - ${user[key]}`);
-}
+// Очікується
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   }),
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   }),
+// );

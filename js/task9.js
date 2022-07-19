3d7c9059b791d5270c5b21dc8966a71da2a51fd8
Den <52295780+josserden@users.noreply.github.com>
2022-07-19 12:34:36 +0300

    add clousure

--- js/task9.js
+++ js/task9.js
@@ -4,16 +4,36 @@
  * Виконайте рефакторинг коду, використовуючи метод для Each і стрілочні функції.
  */
 
-const calculateAverage = (...args) => {
-  let total = 0;
+// const calculateAverage = (...args) => {
+//   let total = 0;
 
-  args.forEach(arg => {
-    total += arg;
-  });
+//   args.forEach(arg => {
+//     total += arg;
+//   });
 
-  return total / args.length;
-};
+//   return total / args.length;
+// };
 
-console.log(calculateAverage(1, 2, 3, 4)); // 2.5
-console.log(calculateAverage(14, 8, 2)); // 8
-console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
+// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calculateAverage(14, 8, 2)); // 8
+// console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
+
+function foo() {
+  let count = 0;
+
+  return function () {
+    return (count += 1);
+  };
+}
+
+const count = foo();
+const count2 = foo();
+
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count2());
+console.log(count2());
+console.log(count2());
+console.log(count2());
